37fd44e4ac47712bcc655bb3676a3bc2e4816206
Ivelin Tchangalov <i.tchangalov@gmail.com>
2023-09-04 17:44:22 -0400

    Initial Commit for Ivelin

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Manuel Ernesto Garcia | Website';
-export const SITE_DESCRIPTION = 'Software Engineer from Cuba • JS • Minimalist • Creando soluciones y compartiendo lo que aprendo.';
+export const SITE_TITLE = 'Ivelin Tchangalov | Website';
+export const SITE_DESCRIPTION = 'Software & Data Engineer • Quality Assurance';
